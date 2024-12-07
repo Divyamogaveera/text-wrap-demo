00581e6e32e353ae26b58b4c12dba62b1b65ae00
Divya Mogaveera <divyashekhar4444@gmail.com>
2024-12-07 23:53:02 +0530

    add relative Position example

--- src/Components/Position.js
+++ src/Components/Position.js
@@ -22,6 +22,32 @@ function Position() {
             <div className="sticky">
                 Sticky Position
             </div>
+            <hr />
+            <div style={{ backgroundColor: "gray", color: "white" }}>
+                <div style={{ border: "2px solid black", width: "300px; height: 300px;" }}>
+                    <p>This is static (default). The element
+                        stays where it normally appears.</p>
+
+                </div>
+                <div style={{ border: "2px solid black; width: 300px", height: "300px", position: "relative" }}>
+                    <p style={{position: "relative", top:"20px", left: "30px", background: "lightblue", color:'blue'}}>
+                        This is relative. I moved 20px down and 30px to the right from my normal position.
+                    </p>
+                </div>
+
+                <div style={{border: "2px solid black", width: "300px", height: "300px"}}>
+                    <p style={{position: "relative", top: "20px", left: "30px", background: "lightblue"}}>
+                        This is relative. I moved 20px down and 30px right.
+                    </p>
+                    <span style={{display:"block" , background: "lightcoral"}}>
+                        I appear where the blue box's original position is reserved.
+                    </span>
+                </div>
+
+
+
+            </div>
+
         </>
     )
 }
